bc1d13df68196fa6be723abf02f2e69e9eb48307
Arda Arslan <arda.arslan@boun.edu.tr>
2022-12-23 23:23:20 -0100

    Anatomy of main.c described.

---
+++ main.c
@@ -0,0 +1,16 @@
+/* https://opensource.com/article/19/5/how-write-good-c-main-function*/
+
+/* main.c */
+/* 0 copyright/licensing */
+/* 1 includes */
+/* 2 defines */
+/* 3 external declarations */
+/* 4 typedefs */
+/* 5 global variable declarations */
+/* 6 function prototypes */
+
+int main(int argc, char *argv[]) {
+/* 7 command-line parsing */
+}
+
+/* 8 function declarations */
