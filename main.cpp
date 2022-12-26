6addbcab662cd9b0d63a698ce6529b96f595f9b9
ardavic <arslanarda2001@gmail.com>
2022-12-26 20:25:35 +0300

    TODO: add multithreading.

--- main.cpp
+++ main.cpp
@@ -7,6 +7,27 @@ std::vector<std::string> parseStringToArray(std::string s);
 
 enum customerStatus {created, ready, waiting, processing};
 
+class Company {
+    private:
+        std::string _name;
+        int _balance;
+        int _transactionCount;
+
+    public:
+        Company(std::string name){
+            _name = name; 
+            _balance = 0;
+            _transactionCount = 0;
+        }
+        int getBalance(){return _balance;}
+        void receivePayment(int receiveAmount){ 
+            _balance += receiveAmount;
+            _transactionCount++;
+        }
+        void toConsole(){ std::cout << "Company " << _name  << " | Balance = " << _balance << " , Transactions = " << _transactionCount << std::endl;}
+
+};
+
 class Customer {
     private:
         int _id;
@@ -25,42 +46,111 @@ class Customer {
             _payAmount = payAmount;
             status = created;
         }
-        void toConsole(){
-            std::cout << "Customer " << _id  << ": [" << _sleepDuration << ","<< _payFromMachineID << ","<< _payToCompany << ","<< _payAmount << ","<< status << "]"<< std::endl;
+        void toConsole(){ std::cout << "Customer " << _id  << ": [" << _sleepDuration << ","<< _payFromMachineID << ","<< _payToCompany << ","<< _payAmount << ","<< "status="<< status << "]"<< std::endl;}
+};
+
+class VendingMachine {
+    private:
+        int _id;
+        int _transactionCount;
+    
+    public:
+        VendingMachine(int id){
+            _id = id;
+            _transactionCount = 0;
+        };
+        void toConsole(){std::cout <<"Vending Machine " << _id << " |" <<" Transactions = " <<  _transactionCount << std::endl;}
+        void processPayment(){_transactionCount++;}
+};
+
+class Transaction{
+    private:
+        int _id;
+        int _issuedByMachineID;
+        std::string _issuedByCompanyName;
+        int _requestedFromCustomerID;
+    public:
+        Transaction(int id,int issuedByMachineID, std::string issuedByCompanyName, int requestedFromCustomerID){
+            _id = id;
+            _issuedByMachineID = issuedByMachineID;
+            _issuedByCompanyName = issuedByCompanyName;
+            _requestedFromCustomerID = requestedFromCustomerID;
         }
+        void toConsole(){ std::cout<<"Transaction "<< _id << " | " << "ByMachine:" << _issuedByMachineID << " , ByCompany:" << _issuedByCompanyName << " , Customer:" <<_requestedFromCustomerID << std::endl ;}
 };
 
 
+
+/*  main() function consists of 4 parts:
+.
+.                                1.COMPANIES
+.                                2.MACHINES
+.                                3.CUSTOMERS
+.                                4.TRANSACTIONS     
+.                
+*/                                 
+
 int main(){
+
+    /*                           --COMPANIES--
+    Create 5 different service provider companies and keep'em in an array.*/
+    Company* COMPANIES[5];
+    Company* Bob = new Company("Bob");                  COMPANIES[0]=Bob;
+    Company* Dave = new Company("Dave");                COMPANIES[1]=Dave;
+    Company* Kevin = new Company("Kevin");              COMPANIES[2]=Kevin;
+    Company* Otto = new Company("Otto");                COMPANIES[3]=Otto;
+    Company* Stuart = new Company("Stuart");            COMPANIES[4]=Stuart;
+
+
+    /*                           --MACHINES--
+    Create 10 different service provider vending machines and keep'em in an array.*/
+    VendingMachine* MACHINES[10];
+    for(int i=0;i<10;i++){
+        VendingMachine* mach = new VendingMachine(i);   
+        MACHINES[i]=mach;
+    }
+
+    /*                           --CUSTOMERS--
+    Create customerCount many Customer object instances as specified in the input file 
+    .                                                     and keep'em in an array.*/
     int customerCount;
     static int customersCreated=0;
 
-    /*Open input file and read first line, integer value of customerCount.*/
-    std::ifstream ReadFile("input1.txt");
-    std::string firstLine;
+    std::ifstream ReadFile("input1.txt");                 //  First line of input file, 
+    std::string firstLine;                                //  specifies customerCount.
     getline(ReadFile, firstLine);
     customerCount = std::stoi(firstLine);
 
-    /*Create array of pointers to Customer object instances*/
-    Customer* customerPtrs[customerCount];
+    Customer* CUSTOMERS[customerCount];                   //  Create array.
 
-    /*Create customerCount many Customer object instances with respect to input file.*/
     for(int i=0; i<customerCount;i++){
+        //Read a new line and parse required info construct object respectively.
+        std::string line; getline(ReadFile, line); std::vector<std::string> lineArray = parseStringToArray(line);
+        int sleepDuration=std::stoi(lineArray[0]); int payFromMachineID=std::stoi(lineArray[1]);
+        std::string payToCompany = lineArray[2]; int payAmount=std::stoi(lineArray[3]);
 
-        /*Read a new line and parse required info.*/
-        std::string line;
-        getline(ReadFile, line);
-        
-        std::vector<std::string> lineArray = parseStringToArray(line);
-        int sleepDuration=std::stoi(lineArray[0]); 
-        int payFromMachineID=std::stoi(lineArray[1]);
-        std::string payToCompany = lineArray[2];
-        int payAmount=std::stoi(lineArray[3]);
-
-        Customer* customerPtr = new Customer(customersCreated++, sleepDuration, payFromMachineID, payToCompany, payAmount);
-        customerPtrs[i] = customerPtr;
-        customerPtrs[i] -> toConsole();
+        Customer* customer = new Customer(customersCreated++, sleepDuration, payFromMachineID, payToCompany, payAmount);
+        CUSTOMERS[i] = customer;
     }
+
+
+    /*                           --TRANSACTIONS--
+    Here simulation runs, prepayments documented as transactions are made. */
+    
+    
+    for(int i=0;i<5;i++)
+        COMPANIES[i] -> toConsole();
+
+    for(int i=0;i<customerCount;i++)
+        CUSTOMERS[i] -> toConsole();
+
+    for(int i=0;i<10;i++)
+        MACHINES[i]-> toConsole();
+
+       
+
+
+
     return 0;
 }
 
@@ -78,6 +168,5 @@ std::vector<std::string> parseStringToArray(std::string s){
         s.erase(0, pos + delimiter.length());
     }
     v[index] = s;
-
     return v;
 } 
