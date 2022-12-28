7408696d6a7ce2b5cacf994556f1ec393df46d74
ardavic <arslanarda2001@gmail.com>
2022-12-28 19:25:58 +0300

    threading added

--- main.cpp
+++ main.cpp
@@ -1,10 +1,17 @@
+/* TODO: Insert Locking Mechanism*/
+/* TODO: Make Thread Wait*/
+
 #include <iostream>
 #include <string>
 #include <vector>
 #include <fstream>
+#include <thread>
 
+void createNewCustomer(int GenerationLoopCounter, int id, int sleepDuration, int payFromMachineID, std::string payToCompany, int payAmount);
+void printDetailsOfCustomerWithIndex(int i);
 std::vector<std::string> parseStringToArray(std::string s);
 
+static int customersCreated=0;
 enum customerStatus {created, ready, waiting, processing};
 
 class Company {
@@ -27,7 +34,6 @@ class Company {
         void toConsole(){ std::cout << "Company " << _name  << " | Balance = " << _balance << " , Transactions = " << _transactionCount << std::endl;}
 
 };
-
 class Customer {
     private:
         int _id;
@@ -38,8 +44,8 @@ class Customer {
         customerStatus status;
 
     public:
-        Customer(int id, int sleepDuration, int payFromMachineID, std::string payToCompany, int payAmount){
-            _id = id;
+        Customer(int sleepDuration, int payFromMachineID, std::string payToCompany, int payAmount){
+            _id = customersCreated++;
             _sleepDuration = sleepDuration;
             _payFromMachineID = payFromMachineID;
             _payToCompany = payToCompany;
@@ -48,7 +54,6 @@ class Customer {
         }
         void toConsole(){ std::cout << "Customer " << _id  << ": [" << _sleepDuration << ","<< _payFromMachineID << ","<< _payToCompany << ","<< _payAmount << ","<< "status="<< status << "]"<< std::endl;}
 };
-
 class VendingMachine {
     private:
         int _id;
@@ -62,7 +67,6 @@ class VendingMachine {
         void toConsole(){std::cout <<"Vending Machine " << _id << " |" <<" Transactions = " <<  _transactionCount << std::endl;}
         void processPayment(){_transactionCount++;}
 };
-
 class Transaction{
     private:
         int _id;
@@ -79,8 +83,6 @@ class Transaction{
         void toConsole(){ std::cout<<"Transaction "<< _id << " | " << "ByMachine:" << _issuedByMachineID << " , ByCompany:" << _issuedByCompanyName << " , Customer:" <<_requestedFromCustomerID << std::endl ;}
 };
 
-
-
 /*  main() function consists of 4 parts:
 .
 .                                1.COMPANIES
@@ -88,13 +90,22 @@ class Transaction{
 .                                3.CUSTOMERS
 .                                4.TRANSACTIONS     
 .                
-*/                                 
+*/
+#define SIZEMAXCUSTOMERS 300
+#define COMPANYCOUNT 5
+#define MACHINECOUNT 10
+
+int customerCount;                                 
+Customer* CUSTOMERS[SIZEMAXCUSTOMERS];                    // Define GLOBAL variables.
+Company* COMPANIES[COMPANYCOUNT];
+VendingMachine* MACHINES[MACHINECOUNT];
 
 int main(){
+    std::thread::id threadID = std::this_thread::get_id();
+    std::cout << "printThreadID: "<< threadID <<" main()\n";
 
     /*                           --COMPANIES--
     Create 5 different service provider companies and keep'em in an array.*/
-    Company* COMPANIES[5];
     Company* Bob = new Company("Bob");                  COMPANIES[0]=Bob;
     Company* Dave = new Company("Dave");                COMPANIES[1]=Dave;
     Company* Kevin = new Company("Kevin");              COMPANIES[2]=Kevin;
@@ -104,7 +115,6 @@ int main(){
 
     /*                           --MACHINES--
     Create 10 different service provider vending machines and keep'em in an array.*/
-    VendingMachine* MACHINES[10];
     for(int i=0;i<10;i++){
         VendingMachine* mach = new VendingMachine(i);   
         MACHINES[i]=mach;
@@ -112,32 +122,59 @@ int main(){
 
     /*                           --CUSTOMERS--
     Create customerCount many Customer object instances as specified in the input file 
-    .                                                     and keep'em in an array.*/
-    int customerCount;
-    static int customersCreated=0;
+    .                                                                  and keep'em in an array.  */
 
-    std::ifstream ReadFile("input1.txt");                 //  First line of input file, 
-    std::string firstLine;                                //  specifies customerCount.
+
+    int customerCount;  
+
+    std::ifstream ReadFile("input1.txt");                 //              First line of input file, 
+    std::string firstLine;                                //              specifies  customerCount.
     getline(ReadFile, firstLine);
     customerCount = std::stoi(firstLine);
 
-    Customer* CUSTOMERS[customerCount];                   //  Create array.
+    std::vector<std::thread> threadsOfCustomers;          //  THREADS processing customer affaires.
 
     for(int i=0; i<customerCount;i++){
+        
         //Read a new line and parse required info construct object respectively.
         std::string line; getline(ReadFile, line); std::vector<std::string> lineArray = parseStringToArray(line);
         int sleepDuration=std::stoi(lineArray[0]); int payFromMachineID=std::stoi(lineArray[1]);
         std::string payToCompany = lineArray[2]; int payAmount=std::stoi(lineArray[3]);
 
-        Customer* customer = new Customer(customersCreated++, sleepDuration, payFromMachineID, payToCompany, payAmount);
-        CUSTOMERS[i] = customer;
-    }
 
+        threadsOfCustomers.push_back(std::thread(createNewCustomer, i,0, sleepDuration, payFromMachineID, payToCompany, payAmount));
+
+    };
+
+    for (auto &th : threadsOfCustomers) 
+        th.join();     
+    /*  void join();
+    .    The function returns when the thread execution has completed.
+    .        This synchronizes the moment this function returns with the completion of all the operations in the thread: 
+    .        This blocks the execution of the thread that calls this function, 
+    .        until the function called on construction returns (if it hasn't yet).                                    */                                        
+    
 
     /*                           --TRANSACTIONS--
     Here simulation runs, prepayments documented as transactions are made. */
     
-    
+
+
+
+
+
+
+
+
+
+
+
+
+    """                             DebugTools                             """
+
+        """//CutAboveLine//"""    
+    /*  
+        """//CutAboveLine//"""
     for(int i=0;i<5;i++)
         COMPANIES[i] -> toConsole();
 
@@ -146,14 +183,23 @@ int main(){
 
     for(int i=0;i<10;i++)
         MACHINES[i]-> toConsole();
-
-       
-
-
+    */
+        """//CutAboveLine//""";
 
     return 0;
 }
 
+void createNewCustomer(int GenerationLoopCounter,int id, int sleepDuration, int payFromMachineID, std::string payToCompany, int payAmount){
+    Customer* customer = new Customer(sleepDuration, payFromMachineID, payToCompany, payAmount);
+    CUSTOMERS[GenerationLoopCounter] = customer;
+    printDetailsOfCustomerWithIndex(GenerationLoopCounter);
+}
+
+void printDetailsOfCustomerWithIndex(int i){
+    std::thread::id threadID = std::this_thread::get_id();
+    std::cout << "printThreadID: "<< threadID <<" customerDetails:\n";
+    CUSTOMERS[i] -> toConsole();
+}
 
 std::vector<std::string> parseStringToArray(std::string s){
     std::vector<std::string> v(4);
